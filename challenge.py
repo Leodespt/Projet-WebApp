a02112775d3cb91b2e2db747290be7726c634a44
leonard.desportes@gmail.com <leodespt@gmail.com>
2023-02-24 15:54:52 +0100

    Challenge class init

--- challenge.py
+++ challenge.py
@@ -1,1 +1,7 @@
-print("hello")
+class Challenge():
+    def __init__(self, id, challenge, value, category, sobriety_score):
+        self.id = id
+        self.challenge = challenge
+        self.value = value
+        self.category = category
+        self.sobriety_score = sobriety_score
